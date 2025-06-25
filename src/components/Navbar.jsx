2064e223a5d78bc6facb86361ae8e2d1af9e63a0
Farhan Fadholy <farhamfadholy870@gmail.com>
2025-06-25 23:56:58 +0700

    responsive web

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const Navbar = () => {
+  const [show, setShow] =useState(false)
+
+  const handleClick = () => {
+    setShow(!show)
+    // console.log(show )
+  }
+
+
+  let menuActive = show ? "left-0" : "-left-full"
+
+  return (
+    <div className="navbar fixed w-full transition-all py-5">
+      <div className="container mx-auto px-4 ">
+        <div className="navbar-box flex items-center justify-between">
+          <div className="logo">
+            <h1 className="text-4xl font-bold">Hanly.</h1>
+          </div>
+          <ul className={`flex lg:gap-14 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:p-0 md:m-0 
+          md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-slate-300 transition-all`}>
+            <li>
+              <a href="#" className="font-medium opacity-75">
+                Beranda
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-medium opacity-75">
+                Tentang Kami
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-medium opacity-75">
+                Layanan
+              </a>
+            </li>
+            <li>
+              <a href="#" className="font-medium opacity-75">
+                Project
+              </a>
+            </li>
+          </ul>
+          <div className="social flex items-center gap-2 ">
+            <a
+              href="#"
+              className="bg-sky-800 px-3 py-3 rounded-full text-white hover:bg-sky-600 transition-all"
+            >
+              Social Media
+            </a>
+            <i className="ri-menu-line text-3xl md:hidden block" onClick={handleClick}></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
