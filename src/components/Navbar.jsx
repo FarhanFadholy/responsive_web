2d60b08e750054a118542de5483a287277936cd5
Farhan Fadholy <farhanfadholy870@gmail.com>
2025-06-27 22:44:59 +0700

    Change the favicon and add an icon to the sidebar.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,46 @@
 import { useState } from "react";
 
 const Navbar = () => {
-  const [show, setShow] =useState(false)
+  const [show, setShow] = useState(false);
 
   const handleClick = () => {
-    setShow(!show)
+    setShow(!show);
     // console.log(show )
-  }
+  };
 
-
-  let menuActive = show ? "left-0" : "-left-full"
+  let menuActive = show ? "left-0" : "-left-full";
 
   return (
     <div className="navbar fixed w-full transition-all py-5">
       <div className="container mx-auto px-4 ">
         <div className="navbar-box flex items-center justify-between">
           <div className="logo">
-            <h1 className="text-4xl font-bold">Hanly.</h1>
+            <h1 className="sm:text-4xl text-xl font-bold">Hanly.</h1>
           </div>
-          <ul className={`flex lg:gap-14 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:p-0 md:m-0 
-          md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-slate-300 transition-all`}>
-            <li>
+          <ul
+            className={`flex lg:gap-14 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:p-0 md:m-0 
+          md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 bg-slate-300 transition-all`}
+          >
+            <li className="flex items-center gap-3">
+              <i className="ri-home-line text-3xl md:hidden block"></i>
               <a href="#" className="font-medium opacity-75">
                 Beranda
               </a>
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-information-line text-3xl md:hidden block"></i>
               <a href="#" className="font-medium opacity-75">
                 Tentang Kami
               </a>
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-settings-3-line text-3xl md:hidden block"></i>
               <a href="#" className="font-medium opacity-75">
                 Layanan
               </a>
             </li>
-            <li>
+            <li className="flex items-center gap-3">
+              <i className="ri-image-line text-3xl md:hidden block"></i>
               <a href="#" className="font-medium opacity-75">
                 Project
               </a>
@@ -48,7 +53,10 @@ const Navbar = () => {
             >
               Social Media
             </a>
-            <i className="ri-menu-line text-3xl md:hidden block" onClick={handleClick}></i>
+            <i
+              className="ri-menu-line text-3xl md:hidden block"
+              onClick={handleClick}
+            ></i>
           </div>
         </div>
       </div>
